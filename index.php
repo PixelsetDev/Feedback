3c0176b18b59b6ef75166c80ae082b54c14dbffe
lewmilburn <lewiscmilburn@gmail.com>
2022-05-21 18:12:44 +0100

    Updated

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
 <?php
+
+ob_start();
+session_start();
+/**
+ * @author      LMWN <contact@lmwn.co.uk>
+ * @copyright   Copyright (c), 2022 LMWN & Lewis Milburn
+ *
+ * This file is a modified version of the demo router provided by https://github.com/bramus/router.
+ */
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+
+require_once 'Boa/Boa.php';
+
+$app = new Boa\App();
+$router = new Boa\Router\Router();
+$SQL = new Boa\Database\SQL();
+
+// Error Handler
+$router->set404(function () {
+    header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
+    exit;
+});
+
+// Before Router Middleware
+$router->before('GET', '/.*', function () {
+    header('X-Powered-By: Boa/Router');
+});
+
+$result = $SQL->Query('SELECT `slug`, `university` FROM `accommodation` WHERE 1;');
+
+// Homepage
+$router->get('/', function () {
+    require_once __DIR__ . '/common/views/homepage.php';
+});
+
+// Thunderbirds are go!
+$router->run();
+
+// EOF
+ob_end_flush();
